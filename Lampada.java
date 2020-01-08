844b555a691ca307a1075ae1148ecbdab2792a83
pithan92 <50022218+pithan92@users.noreply.github.com>
2020-01-08 07:32:52 -0300

    Update Lampada.java

--- Lampada.java
+++ Lampada.java
@@ -24,4 +24,13 @@ package execicio5;
 				System.out.println("Lâmpada ligada");
 			}else {System.out.println("Lâmpada desligada");}
 		};
-	};
+
+		public static void main(String[] args) {	
+			Lampada l1 = new Lampada();
+			l1.ligar();
+			l1.desligar();
+			l1.imprimir();
+			
+		}
+
+	}
