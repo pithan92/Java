f2c3d190ce8d0b49cbd5ed0ae46eb437cf16f47c
pithan92 <50022218+pithan92@users.noreply.github.com>
2020-01-09 22:08:27 -0300

    Add files via upload

---
+++ AplicacaoLampada.java
@@ -0,0 +1,11 @@
+package execicio5;
+	public class AplicacaoLampada {
+		public static void main(String[] args) {	
+			Lampada l1 = new Lampada();
+			l1.ligar();
+			l1.imprimir();
+			l1.desligar();
+			l1.imprimir();
+			
+		}
+	}
